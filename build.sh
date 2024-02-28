49f24f7b0484be59cb231d51b5264c129d5ea7e3
mrmcshane <Andy.McShane@test-and-trace.nhs.uk>
2024-02-28 09:58:11 +0000

    fixing for podman

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 green='\033[32m';
 no_colour='\033[0m';
 
-echo -e "${green}[killing whatever is on port 80]${no_colour}"
-docker kill $(docker ps -f publish=80 -q) # kill whatever is on port 80
+echo -e "${green}[killing whatever is on port 8080]${no_colour}"
+podman kill $(podman ps -f publish=8080 -q) # kill whatever is on port 80
 
-echo -e "${green}[build docker image]${no_colour}"
-docker build . -t auto_mkdocs
+echo -e "${green}[build podman image]${no_colour}"
+podman build . -t auto_mkdocs
 
-echo -e "${green}[run docker image]${no_colour}"
-docker run -d -p 80:80 auto_mkdocs
+echo -e "${green}[run podman image]${no_colour}"
+podman run -d -p 8080:80 auto_mkdocs
 
 echo -e "${green}[opening browser]${no_colour}"
-/usr/bin/open -a "/Applications/Google Chrome.app" "http://localhost"
+/usr/bin/open -a "/Applications/Google Chrome.app" "http://localhost:8080"
